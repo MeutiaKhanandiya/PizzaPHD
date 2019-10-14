2c6f9eba60ddb34914cf4a069d28b217cca0a2cc
MeutiaKhanandiya <khanandyam@gmail.com>
2019-10-14 13:39:10 +0700

    Menyelesaikan Tugas UTS

---
+++ app/src/main/java/com/meutia/pizzaphd/Activities/MainActivity.java
@@ -0,0 +1,55 @@
+package com.meutia.pizzaphd.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.content.ClipData;
+import android.os.Bundle;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.meutia.pizzaphd.Fragments.ItemFragment;
+import com.meutia.pizzaphd.Fragments.OrderFragment;
+import com.meutia.pizzaphd.R;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadFragment(new OrderFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.action_item:
+                fragment = new ItemFragment();
+                break;
+            case R.id.action_order:
+                fragment = new OrderFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
